c04695edbce9a447034df0746f14b6e4dfb1fb4e
Ritesh Patidar <riteshpatidar2499@gmail.com>
2023-03-27 00:26:46 +0530

    all information retrieved

--- format_output_from_xml.py
+++ format_output_from_xml.py
@@ -5,17 +5,44 @@ tree = ET.parse('output.xml')
 root = tree.getroot()
 TITLE_output = ''
 
+namespace = root.tag.split('}')[0].strip('{')
+print(namespace)
 
 for child in root:
     if re.search('title', child.tag):
         TITLE_output = child.text
-    if re.search('creator', child.tag):
+        print("Title: ",TITLE_output)
+        print("=====================================")
+    if re.match("{" + namespace + "}creator", child.tag):
+    # if re.search('creator', child.tag):
         for personname in child:
-            if re.search('personname', personname.tag):
-                for data in personname:
-                    print(data.text)
-        # print(child.find('*').text)
-        # get entire data from creator
+            if re.match("{" + namespace + "}personname", personname.tag):
+            # if re.search('personname', personname.tag):
+                # print(personname.text)
+                for c in personname.itertext():
+                    if re.search('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', c):
+                        print(c)
+                        print("=====================================")
+                    else:
+                        print(c)
+                    # if c.text != None:
+                    #     if re.search('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', c.text):
+                    #         print(c.text)
+                    #         print("=====================================")  
+                    # else:   
+                    #     if c.tail:                   
+                    #         print(c.tail)
+
+for child in root:
+    if re.match("{" + namespace + "}section", child.tag):
+        for key in child.attrib:
+            if re.match('^{[^{}]*}id', key):
+                print("ID:",child.attrib[key])
+        for subchild in child.iter():
+            if subchild.tag == "{" + namespace + "}title":
+                for c in subchild.itertext():
+                    print(c)
+
 
         
 
